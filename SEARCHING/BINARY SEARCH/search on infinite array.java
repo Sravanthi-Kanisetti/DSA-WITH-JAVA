6988caa9d5d42b87572300f7308a3f023202e5f1
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-08-03 13:35:59 +0530

    Create search on infinite array.java

---
+++ SEARCHING/BINARY SEARCH/search on infinite array.java
@@ -0,0 +1,50 @@
+
+
+public class Main{
+    public static void main(String[] args){
+        int[] nums={1,3,4,7,8,17,19,23,26,28,37,55,56,66};
+        int target=37;
+        System.out.println("the result came is "+measure(nums,target));
+        
+    }
+    
+    static int measure(int[] nums,int target){
+        int start=0;
+        int end=1;
+        
+        while(target>nums[end]){
+            int temp=end+1;
+            end=end+(end-start+1)*2;
+            start=temp;
+        }
+        
+        return binarysearch(nums,target,start,end);
+    }
+    
+    
+    static int  binarysearch(int[] nums,int target,int start,int end){
+        while(start<=end){
+        int mid=start+(end-start)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(target>nums[mid]){
+            start=mid+1;
+        }
+        if(target<nums[mid]){
+            end=mid-1;
+        }
+        
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+
+
+
+
