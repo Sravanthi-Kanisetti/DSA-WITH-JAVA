50adb81bed6f71b19ca33b5ad200928882bcf488
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-08-04 21:55:02 +0530

    Update leetcode 33 my own code.java

--- SEARCHING/BINARY SEARCH/leetcode 33 my own code.java
+++ SEARCHING/BINARY SEARCH/leetcode 33 my own code.java
@@ -1,55 +1,73 @@
 
-public class Main{
-    public static void main(String [] args){
-       int[] nums={4,5,6,7,0,1,2};
-       int target=3;
-       int peak=peakIndexInMountainArray(nums);
-       int fira=-1;
-       if (nums[0]<=target && target<=nums[peak]){
-           fira=binarysearch(nums,target,0,peak);
-       }
-       else{
-           fira=binarysearch(nums,target,peak,nums.length-1,);
-       }
-       
-       System.out.println("the target is at index "+fira);
-       
-    }
-    
-    
-       static int peakIndexInMountainArray(int[] nums) {
-           int start=0;
-           int end=nums.length-1;
-           while(start<end){
-               int mid=start+(end-start)/2;
-               if(nums[mid]>nums[mid+1]){
-                   end=mid;
-               }
-               else{
-                   start=mid+1;
-               }
-           }
-           return start;
-    }
-    
-    
-      
-    static int binarysearch(int[] nums,int target,int start,int end){
-        while(start<=end){
-            int mid=start+(end-start)/2;
-                     if(nums[mid]==target){
-                                   return mid;
-                    }
-                        if(nums[mid]>target){
-                            end=mid-1;
-                        }
-                        else{
-                            start=mid+1;
-                        }
-            }
-        return -1;
-    }
-      
-        
-    
-}
+public class Main
+{
+	public static void main(String[] args) {
+		int[] nums={3,5,1};
+		int target=3;
+		System.out.println(search(nums,target,0,nums.length-1));
+	
+	}
+	
+	
+        	static  int search(int[] nums,int target,int start,int end){
+        	    int pivot=findPivot(nums);
+        	    System.out.println(pivot);
+        	    if(pivot==-1){
+        	        return binarySearch(nums,target,start,end);
+        	    }
+        	    if(nums[pivot]==target){
+        	        return pivot;
+        	    }
+        	    if(target>=nums[start]){
+        	        return binarySearch(nums,target,start,pivot);
+        	    }
+        	    else{
+        	        return binarySearch(nums,target,pivot+1,end);
+        	    }
+        	}
+	    
+	
+	
+  static int findPivot(int[] nums){
+      int start=0;
+      int  end=nums.length-1;
+      while(start<=end){
+          int mid=start+(end-start)/2;
+          if(mid<end && nums[mid]>nums[mid+1] ){
+              return mid;
+          }
+          if( mid>start && nums[mid]<nums[mid-1] ){
+              return mid-1;
+          }
+          if(nums[start]<nums[mid]){
+              start=mid+1;
+          }
+          else{
+              end=mid-1;
+          }
+      }
+      return -1;
+  }	
+  
+  
+  
+  
+              
+              static int binarySearch(int[] nums,int target,int start,int end){
+                        	    while(start<=end){
+                        	        int mid=start+(end-start)/2;
+                        	        
+                        	        if(target>nums[mid]){
+                        	            start=mid+1;
+                        	        }
+                        	        else if(target<nums[mid]){
+                        	            end=mid-1;
+                        	        }
+                        	        else{
+                        	            return mid;
+                        	        }
+                        	    }
+            	    return -1;
+                      
+                  }
+  }
