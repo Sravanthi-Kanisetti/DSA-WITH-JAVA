f7adf39cfbec5f25f9d7aa9a390cffb44e68e01e
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-08-14 06:40:11 +0530

    Create 2dsearch.java

---
+++ SEARCHING/BINARY SEARCH/2dsearch.java
@@ -0,0 +1,36 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int[][] nums={
+		    {1,2,3,4,5},
+		    {6,7,8,9,10},
+		    {11,12,13,14,15},
+		    {16,17,18,19,20},
+		    {26,27,28,29,30}
+		};
+		int target=17;
+		System.out.println(Arrays.toString(search(nums,target)));
+		System.out.println("length is=>"+nums.length);
+	}
+	static int[] search(int[][] nums,int target){
+	    int r=0;
+	    int c=nums.length-1;
+	    while(c>=0 && r<nums.length){
+	        if(nums[r][c]==target){
+	            return new int[]{r,c};
+	        }
+	        if(nums[r][c]<target){
+	            r++;
+	        }
+	        else{
+	            c++;
+	        }
+	    }
+	    
+	    
+	  return new int[]{-1,-1};
+	    
+	}
+}
