82ee71f76b139db5e4919ed1940a094578863266
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-08-20 05:13:25 +0530

    Create kk's bubble sort.java

---
+++ SORTING/kk's bubble sort.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   int[] nums={-5,3,-2,1,4};
+	   bubble(nums);
+	   System.out.println(Arrays.toString(nums));
+	   
+	}
+	
+	static void bubble(int[] nums){
+	    for(int i=0;i<nums.length;i++){
+	        boolean swapped=false;
+	        for(int j=1;j<nums.length-i;j++){
+	            if(nums[j]<nums[j-1]){
+	                int temp=nums[j];
+	                nums[j]=nums[j-1];
+	                nums[j-1]=temp;
+	                swapped=true;
+	            }
+	        }
+	        if(!swapped){
+	            break;
+	        }
+	    }
+	}
+}
