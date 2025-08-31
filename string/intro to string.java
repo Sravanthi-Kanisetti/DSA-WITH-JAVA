0d4eeb98c3f684d8dfa1b7ed1987c029baf71884
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-08-31 21:57:49 +0530

    Update intro to string.java

--- string/intro to string.java
+++ string/intro to string.java
@@ -15,5 +15,27 @@ public class Main
 		System.out.println(aa==bb);
 		//when u want to comapre two string values use euals method not ==
 		System.out.println(aa.equals(bb));
+
+
+			System.out.println(new int[]{9,7,5,3,2});
+		
+		System.out.println(Arrays.toString(new int[]{9,7,5,3,2}));
+		
+		float num=43.78976f;
+	//	System.out.printf("Formatted number si %.4f",num);
+		System.out.println(Math.PI);
+	//	System.out.printf("Pie %.3f",Math.PI);
+		String name="sravanthi";
+		String goal="Director";
+	//	System.out.printf("My name is %s and i want to become %s",name,goal);
+		
+		
+		
+		System.out.println('a'+'b');
+		
+			System.out.println("a"+"b");
+				System.out.println('a'+3);
+				System.out.println((char)100);
+					System.out.println("a"+89);
 	}
 }
